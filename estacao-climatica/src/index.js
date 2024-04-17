0b4199582eba9c2d678cec0d7ad64a1fd68edc3a
TodoBugado <rafaelsouzaaguiar10@gmail.com>
2024-04-16 22:22:54 -0300

    fix(ap05): altera o icone de outono

--- estacao-climatica/src/index.js
+++ estacao-climatica/src/index.js
@@ -22,7 +22,7 @@ class App extends React.Component{
     icones = {
         'Verão': 'sun',
         'Inverno': 'snowflake',
-        'Outono': 'canadian-maple-flower',
+        'Outono': 'leaf',
         'Primavera': 'tree'
     }
 
