f51543924ac6b3f3e7e2595b77c70264dcc5fa31
TodoBugado <rafaelsouzaaguiar10@gmail.com>
2024-05-05 20:29:11 -0300

    feat(ap07): desbrava a secao 2.3
    - inicia implementacao do busca
    - tenta ajustar botao e campo de entrada de texto lado a lado responsivamente e falha miseravelmente
    - obs: alunos vao corrigir :)

---
+++ busca-foto/src/components/Busca.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { IconField } from 'primereact/iconfield'
+import { InputIcon } from 'primereact/inputicon'
+import { InputText } from 'primereact/inputtext'
+import { Button } from 'primereact/button'
+
+export default class Busca extends Component {
+    render() {
+        return (
+            <div className='grid'>
+                <div className={`${this.props.classNameInputText}`}>
+                    <IconField
+                        iconPosition='left'>
+                        <InputIcon
+                            className="pi pi-search" />
+                        <InputText
+                            className='w-full h-full' 
+                            placeholder={this.props.dica}
+                            />
+                    </IconField>
+                </div>
+                <Button
+                    label='Buscar'
+                    className={`${this.props.classNameButton}`}/>
+            </div>
+        )
+    }
+}
+Busca.defaultProps = {
+    dica: 'O que deseja buscar?'
+}
